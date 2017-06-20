623641767fc35f577dda7c7049993838cb11fefd
Erin Bennett <erindb@stanford.edu>
2017-06-20 12:17:15 -0700

    skeleton of warmup model

---
+++ models/warmup_model1/warmup_model1.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+"""
+Warmup model for preparing for AVEC17 challenge.
+3-class classifier on Desmond's emotion rating data.
+"""
+
+# read in datapoints
+
+# create datasets for k-fold cross-validation
+
+# set up classifier
+
+# for each fold:
+
+# ... train classifier
+
+# ... compute accuracy
+
+# compute overall accuracy
