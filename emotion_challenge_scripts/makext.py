e371ac98ec4d138ce0c999a957c3b6dfaa17dd8b
Erin Bennett <erindb@stanford.edu>
2017-07-03 15:48:55 -0700

    give get_num_timesteps parameters

--- emotion_challenge_scripts/makext.py
+++ emotion_challenge_scripts/makext.py
@@ -60,16 +60,39 @@ def make_xt(timestep, pNum, dataset, split="train", length_of_timestep=100):
     return torch.from_numpy(xt)
 
 
-# fix me!!!
-def get_num_timepoints(pNum, dataset, split="train", length_of_timestep=100):
-    return None
+"""
+Each participant has a different number of timesteps
+
+pNum - subject number (starts at 1)
+dataset - data directory for specific challenge
+split - train, valid, test
+length_of_timestep - in ms
+"""
+def get_num_timesteps(pNum, dataset, split="train", length_of_timestep=100):
+    assert(split in ["train", "valid", "test"])
+
+    finest_granularity_filename = os.path.join(dataset,
+                                               "audio_features/",
+                                               make_name(pNum, split="train"))
+    last_line = open(finest_granularity_filename).readlines()[-1]
+    max_time_in_seconds = float(last_line.split(";")[1])
+    max_time_in_ms = max_time_in_seconds*1000
+    num_full_intervals = int(max_time_in_ms) / int(length_of_timestep)
+    return num_full_intervals + 1
+
 
 def run_tests():
     assert(1756==get_num_timepoints(1, "../data/AVEC_17_Emotion_Sub-Challenge"))
 run_tests()
 
+
+def run_tests():
+    assert(1756==get_num_timepoints(1, "../data/AVEC_17_Emotion_Sub-Challenge"))
+run_tests()
+
+
 # Runs t = 0, participant 1 as an example
 if __name__ == '__main__':
     print(make_xt(0, 1, "../data/AVEC_17_Emotion_Sub-Challenge"))
-    print(get_num_timepoints(1, "../data/AVEC_17_Emotion_Sub-Challenge"))
+    print(get_num_timesteps(1, "../data/AVEC_17_Emotion_Sub-Challenge"))
 
