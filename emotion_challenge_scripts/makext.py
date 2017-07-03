5e8cf45c1cf79f6bf4c8dae1f15fc5ce1822ea89
Erin Bennett <erindb@stanford.edu>
2017-07-03 14:44:54 -0700

    fix bug in API

--- emotion_challenge_scripts/makext.py
+++ emotion_challenge_scripts/makext.py
@@ -16,7 +16,7 @@ import load_features
 
 
 # Returns a string of the filename given the participant number
-def make_name(pNum, ):
+def make_name(pNum, split="train"):
     pStr = str(pNum)
     if pNum < 10:
         pStr = '0' + pStr
@@ -32,11 +32,11 @@ def get_index(t, reader, f):
 
 
 # Makes x(t) by taking all features and concatenating them into 1 torch Tensor.
-def make_xt(time, pNum, dataset):
+def make_xt(time, pNum, dataset, split="train"):
     xt = np.float64([])
     folders = [folder for folder in os.listdir(dataset) if 'feature' in folder]
     for folder in folders:
-        fname = make_name(pNum)
+        fname = make_name(pNum, split)
         fullName = os.path.join(dataset, folder, fname)
         with open(fullName, 'r') as file:
             reader = csv.reader(file, delimiter=';')
@@ -45,10 +45,10 @@ def make_xt(time, pNum, dataset):
     return torch.from_numpy(xt)
 
 
-def get_num_timepoints(pNum, dataset):
-    return load_features.get_num_lines(os.path.join(dataset, make_name(pNum)))
+def get_num_timepoints(pNum, dataset, split="train"):
+    return load_features.get_num_lines(os.path.join(dataset, make_name(pNum, split)))
 
 
 # Runs t = 0, participant 1 as an example
 if __name__ == '__main__':
-    print(make_xt(0, 1))
+    print(make_xt(0, 1, "../data/AVEC_17_Emotion_Sub-Challenge"))
